1fb64b82a7179bfac9d885580cf058ffc4d3c578
Saifullah Jaialni <sjailani@berkeley.edu>
2022-07-13 00:53:37 -0700

    push

--- public/js/my-drawing-app.js
+++ public/js/my-drawing-app.js
@@ -1,30 +1,137 @@
 /* The code for our drawing application! 
 Feel free to delete any/all of it and replace with your own functionality. */
 
+function Brush(style) {
+    this.style = style
+    this.onMouseDown = function (event) {
+        path = new Path();
+        path.style = this.style;
+        path.add(event.point);
+        this.path = path;
+    }
+
+    this.onMouseDrag = function (event) {
+        this.path.add(event.point);
+    }
+}
+
+function Rectangle(style) {
+    this.style = style
+    this.onMouseDown = function (event) {
+        this.start = event.point;
+        this.end = event.point;
+        this.shape = new Shape.Rectangle(this.start, this.end);
+        this.shape.style = this.style;
+
+
+    }
+
+    this.onMouseDrag = function (event) {
+        this.shape.remove();
+        this.end = event.point;
+        this.shape = new Shape.Rectangle(this.start, this.end);
+        this.shape.style = this.style;
+    }
+}
+
+function Ellipse(style) {
+    this.style = style
+
+    this.onMouseDown = function (event) {
+        this.start = event.point;
+        this.end = event.point;
+        var rect = new Shape.Rectangle(this.start, this.end);
+        this.shape = new Shape.Ellipse({
+            point: this.start,
+            size: rect.size,
+            matrix: rect.matrix,
+        });
+        this.shape.style = this.style;
+
+
+    }
+
+    this.onMouseDrag = function (event) {
+        this.shape.remove();
+        this.end = event.point;
+        var rect = new Shape.Rectangle(this.start, this.end);
+        this.shape = new Shape.Ellipse({
+            point: this.start,
+            size: rect.size,
+            matrix: rect.matrix
+        });
+        this.shape.style = this.style;
+    }
+}
+
+
+function Line(style) {
+    this.style = style
+
+    this.onMouseDown = function (event) {
+        this.start = event.point;
+        this.end = event.point;
+        this.shape = new Path.Line(this.start, this.end);
+        this.shape.style = this.style;
+
+
+    }
+
+    this.onMouseDrag = function (event) {
+        this.shape.remove();
+        this.end = event.point;
+        this.shape = new Path.Line(this.start, this.end);
+        this.shape.style = this.style;
+    }
+}
+
+function Select() {
+
+    this.onMouseDown = function (event) {
+        var res = project.hitTest(event.point);
+        if(res != null){
+            this.item = res.item;
+            this.box = this.item.bounds.clone();
+            this.box.style = {
+                strokeColor: "blue",
+                
+            }
+        }
+        
+        
+    }
+
+    this.onMouseDrag = function (event) {
+        
+    }
+}
+
+var v = new Rectangle({
+    strokeWidth: 10,
+    strokeColor: "black",
+})
+var s = new Select();
+
+var b = v;
+
 var path;
 var currentColor = 'black'
 var currentWidth = 5
 
-tool.onMouseDown = function(event) { //This code in this function is called whenever the mouse is clicked.
-    path = new Path();     // Create a new path each time.
-    path.add(event.point);
-    path.strokeColor = currentColor;
-    path.strokeWidth = currentWidth;
-    console.log(event.point); //this commands log to the Console the coordinates of the mouse click. Feel free to delete it! 
+tool.onMouseDown = function (event) { //This code in this function is called whenever the mouse is clicked.
+    b.onMouseDown(event);
 }
-tool.onMouseDrag = function(event) {
-    path.add(event.point); //Add points to the path as the user drags their mouse.
+tool.onMouseDrag = function (event) {
+    b.onMouseDrag(event);
 }
 
-$('#default').on('click', function (e) { //jquery click event code for our "pencil" button.
-    currentColor = 'black';  //change the color to black
-    currentWidth = 5;       //change the width to 5
-})
-$('#thick-green').on('click', function (e) { //jquery button click code for our "green paint" button.
-    currentColor = 'green';
-    currentWidth = 15;
-})
-$('#eraser').on('click', function (e) { //jquery button click code for our eraser button.
-    currentColor = 'white';
-    currentWidth = 20;
-})
+var curr = v;
+var next = b;
+
+$("#default").click(function(){
+    b = s;
+});
+
+$("#thick-green").click(function(){
+    b = v;
+});
